df1519dfe2c8b7f6f6d755ed027f401cc5fd88ec
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2024-03-12 09:22:12 +1100

    landing page 1/2 done & integrated shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,80 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper'
+import { buttonVariants } from '@/components/ui/button'
+import { ArrowRight } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <MaxWidthWrapper className="mb-12 mt-28 flex flex-col items-center justify-center text-center sm:mt-40">
-      <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-        <p className="text-sm font-semibold text-gray-700">
-          GTD Is Now Available
+    <>
+      <MaxWidthWrapper className="mb-12 mt-28 flex flex-col items-center justify-center text-center sm:mt-40">
+        <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
+          <p className="text-sm font-semibold text-gray-700">
+            GTD Is Now Available
+          </p>
+        </div>
+        <h1 className="max-w-4xl text-5xl font-bold md:text-6xl lg:text-7xl">
+          Unlock 🔓 Your{' '}
+          <span className="text whitespace-nowrap underline">
+            Full Potential
+          </span>{' '}
+          with Our Get-Things-Done App
+        </h1>
+        <p className="mt-5 max-w-prose text-zinc-500 sm:text-lg">
+          GTD Allows you to organise your life with a simple{' '}
+          <span className="font-bold">
+            Capture, Process, Organise, Review, Engage
+          </span>
+          . It&#39;s a simple, yet powerful tool that helps you to stay on top
+          of your tasks and projects.
         </p>
+
+        <Link
+          className={buttonVariants({
+            variant: 'secondary',
+            size: 'lg',
+            className: 'my-8',
+          })}
+          href={'/dashboard'}
+          target="_blank"
+        >
+          Get Started <ArrowRight className="ml-2 h-5 w-5" />
+        </Link>
+      </MaxWidthWrapper>
+
+      {/* value proposition section */}
+      <div>
+        <div className="relative isolate">
+          <div
+            aria-hidden="true"
+            className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          >
+            <div
+              style={{
+                clipPath:
+                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+              }}
+              className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875]"
+            />
+          </div>
+
+          <div>
+            <div className="mx-auto max-w-6xl px-6 lg:px-8">
+              <div className="mt-16 flow-root sm:mt-24">
+                <div className="lg:rounded-2-xl -mt-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:p-4">
+                  <Image
+                    src="/dashboard-preview.png"
+                    alt="Dashboard Preview"
+                    width={1364}
+                    height={866}
+                    className="rounded-md bg-white/5 p-2 shadow-2xl ring-1 ring-gray-900/10 sm:p-8 md:p-20"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </MaxWidthWrapper>
+    </>
   )
 }
